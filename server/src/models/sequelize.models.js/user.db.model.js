d2e247a82619b5db4823f238a43978e29be095c9
CyrilPonsan <cyril.ponsan@gmai.com>
2023-01-13 07:44:03 +0100

    user model

---
+++ server/src/models/sequelize.models.js/user.db.model.js
@@ -0,0 +1,42 @@
+//  table 'user' (aka admin)
+
+const sequelize = require("sequelize");
+
+function userModel(sequelize, DataTypes) {
+  return sequelize.define(
+    "user",
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: { msg: "Cet email n'est pas disponible." },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      nom: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      prenom: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      roles: {
+        type: DataTypes.JSON,
+        allowNull: false,
+      },
+    },
+    {
+      hooks: {
+        beforeCreate: function (user) {
+          user.nom = user.nom.toLowerCase();
+          user.fonction = user.fonction.toLowerCase();
+        },
+      },
+    }
+  );
+}
+
+module.exports = userModel;
