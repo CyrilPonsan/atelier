5e9ce9ea23d436e6fea3bc79175d00536c3c5d76
CyrilPonsan <cyril.ponsan@gmail.com>
2023-01-17 13:01:49 +0100

    tests unitaires

--- server/src/routes/auth.router/auth.spec.js
+++ server/src/routes/auth.router/auth.spec.js
@@ -4,21 +4,42 @@ require("dotenv").config();
 
 const { initDB } = require("../../services/sequelize");
 
+let credentials = {
+  username: "tech@atelier.fr",
+  password: "toto",
+};
+
 describe("API", () => {
   beforeAll(async () => {
     await initDB();
   });
   describe("Test POST /login", () => {
-    const credentials = {
-      username: "tech@atelier.fr",
-      password: "toto",
-    };
     test("réponse attendue : 200", async () => {
-      const response = await request(app)
-        .post("/v1/auth/")
-        .send(credentials)
-        .expect("Content-Type", /json/)
-        .expect(200);
+      const response = await request(app).post("/v1/auth/").send(credentials);
+      expect(200);
+    });
+  });
+  describe("Test POST /login error", () => {
+    credentials.password = "1234";
+    test("réponse attendue : 400", async () => {
+      const reponse = await request(app).post("/v1/auth/").send(credentials);
+      expect(400);
+    });
+  });
+  describe("Test POST /login error", () => {
+    credentials.username = "toto@toto.fr";
+    credentials.password = "toto";
+    test("réponse attendue : 400", async () => {
+      const reponse = await request(app).post("/v1/auth/").send(credentials);
+      expect(400);
+    });
+  });
+  describe("Test POST /login error", () => {
+    credentials.username = "toto";
+    credentials.password = "toto";
+    test("réponse attendue : 400", async () => {
+      const reponse = await request(app).post("/v1/auth/").send(credentials);
+      expect(400);
     });
   });
 });
